3dfcc05664ffde9de749d247d4edfcff0f0db13d
Ekko-Man <monkeywing001@gmail.com>
2020-10-24 19:43:38 +0800

    Add POST and READ function

--- BulletinBoardClient.py
+++ BulletinBoardClient.py
@@ -11,9 +11,47 @@ def connect_to_server(ser_ip, ser_port):
         server_address = (ser_ip, ser_port)
         sock.connect(server_address)
     except:
-        return "Connect status: : No route to host"
+        print("Connect status: : No route to host")
+        sys.exit()
     else:
-        return "Connect status: success"
+        print("Connect status: success")
+        return sock
+
+
+def post_to_server(sock):  # handle recive OK
+    typing = True
+    message = ""
+
+    while typing:
+        line_input = input()
+        print(line_input)
+
+        message += line_input + "\n"
+
+        if line_input == ".":
+            typing = False
+
+
+    try:
+        # Send data
+        sock.sendall(message)
+
+        data = sock.recv(1024)
+        print("server: ", data)
+    except:
+        print("There are some issue between client and server")
+
+
+def read_to_server(sock):  # handle data recived from server (print it)
+    try:
+        # Send data
+        message = 'READ'
+        sock.sendall(message)
+
+        data = sock.recv(1024)
+        print("server: ", data)
+    except:
+        print("There are some issue between client and server")
 
 
 if __name__ == '__main__':
@@ -26,6 +64,22 @@ if __name__ == '__main__':
     else:
         server_ip = server_ip_port
 
-    print(connect_to_server(server_ip, server_port))
+    sock = connect_to_server(server_ip, server_port)
 
+    socket_connect = True
 
+    while socket_connect:
+        method_comment = input()
+        print("client: ", method_comment)
+
+        if method_comment == "POST":
+            read_to_server(sock)    # read user input and send it to the server.
+        elif method_comment == "READ":
+            read_to_server(sock)    # Send the READ message to server and print out the data.
+        elif method_comment == "QUIT":
+            sock.close()    # Close the socket connect between client and server.
+            socket_connect = False
+        else:
+            pass
+
+    sys.exit()
