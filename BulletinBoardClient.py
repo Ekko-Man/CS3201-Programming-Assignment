b948ea1c614e64db818b1d0bef1774f474a162b1
Ekko Man <48004466+Ekko-Man@users.noreply.github.com>
2020-10-28 04:26:15 +0800

    Update BulletinBoardClient.py
    Fixed bug

--- BulletinBoardClient.py
+++ BulletinBoardClient.py
@@ -2,80 +2,78 @@ import socket
 import sys
 
 
-def connect_to_server(ser_ip, ser_port):
+def connect_to_server(ser_ip, ser_port):  # Function to connect to server
     try:
-        # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Some socket setting
+        sock.settimeout(3)
 
-        # Connect the socket to the port where the server is listening
         server_address = (ser_ip, ser_port)
-        sock.connect(server_address)
-    except:
-        print("Connect status: fail")
+        sock.connect(server_address)  # Connect to the server
+    except Exception as e:  # handle error occur
+        print(e)
+        sock.close()
         sys.exit()
     else:
         print("Connect status: success")
         return sock
 
 
-def post_to_server(sock):  # handle recive OK
+def receive_message(sock):  # Function to receive data from server
+    loop_msg = 1
+    had_receive_msg = False
+    while loop_msg:  # Loop all message send by server
+        try:
+            data = sock.recv(1024)
+            print("server: ", data.decode())
+            had_receive_msg = True
+            if not data:
+                return
+        except socket.timeout:
+            if had_receive_msg:  # Check normal
+                return
+            elif not had_receive_msg:  # Handle hanging problem since the server has not sent data back
+                print("Socket timeout don't have any message receive from server.")
+                sock.close()
+                sys.exit()
+
+
+def post_to_server(sock):  # Handle POST command
     typing = True
-    message = ""
+    message = "POST\n"
 
     while typing:  # Loop input text until "."
         line_input = input()
-        print(line_input)
-
+        print("client: ", line_input)
         message += line_input + "\n"
-
         if line_input == ".":
             typing = False
 
     try:
-        sock.sendall(message)  # Send data
-
-        data = sock.recv(1024)
-        print("server: ", data)
-    except:  # Catch error when the message cant send to server
-        print("There are some issue between client and server")
-
-
-def read_from_server(sock):  # handle data recived from server (print it)
-    try:
-        message = 'READ'
-        sock.sendall(message)  # Send data
-
-        data = sock.recv(1024)
-        print("server: ", data)
-    except:  # Catch error when the message cant send to server
-        print("There are some issue between client and server")
+        sock.sendall(message.encode())  # Send data
+        receive_message(sock)  # Receive and print data from server
+    except Exception as e:  # Catch error when the message cant send to server
+        print(e)
+        sock.close()
+        sys.exit()
 
 
-def quit_the_server(sock):  # handle data recived from server (print it)
-    try:
-        message = 'QUIT'
-        sock.sendall(message)  # Send data
+def read_quit_others_command_to_server(sock, command):  # Handle READ, QUIT, and other not exist command
+    message = command.encode()
 
-        data = sock.recv(1024)
-        print("server: ", data)
-        sock.close()  # Close the socket connect between client and server.
-    except:
-        print("There are some issue between client and server, ")
-
-
-def others_to_server(sock, message):  # handle data recived from server (print it)
     try:
         sock.sendall(message)  # Send data
+        receive_message(sock)  # Receive and print data from server
 
-        data = sock.recv(1024)
-        print("server: ", data)
-    except:
-        print("There are some issue between client and server, ")
+        if command == "QUIT":
+            sock.close()  # Close the socket connect between client and server.
+    except Exception as e:
+        print(e)
+        sock.close()
+        sys.exit()
 
 
 if __name__ == '__main__':
     server_port = 16000
-
     server_ip_port = input("Input the IP address\n")  # 127.0.0.1 16000
 
     if " " in server_ip_port:
@@ -84,23 +82,18 @@ if __name__ == '__main__':
         server_ip = server_ip_port
 
     print("IP Address: ", server_ip, "\tPort Number: ", server_port)
-
-    sock = connect_to_server(server_ip, server_port)
-
+    sock = connect_to_server(str(server_ip), int(server_port))
     socket_connect = True
 
-    while socket_connect:
+    while socket_connect:  # Loop handle command from the user until QUIT
         input_command = input()
         print("client: ", input_command)
 
         if input_command == "POST":
             post_to_server(sock)  # read user input and send it to the server.
-        elif input_command == "READ":
-            read_from_server(sock)  # Send the READ message to server and print out the data.
-        elif input_command == "QUIT":
-            quit_the_server(sock)
-            socket_connect = False
         else:
-            others_to_server(sock, input_command)
+            read_quit_others_command_to_server(sock, input_command)  # Handle READ, QUIT, and other not exist command
+            if input_command == "QUIT":  # End the loop when QUIT
+                socket_connect = False
 
     sys.exit()
